d6f010e2c596bcfd5eae3f88d37424760deccabc
VaibhavRajpoot <vaibhavrajpoot46gmail.com>
2023-12-17 19:07:24 +0530

    Post request added

--- go.mod
+++ go.mod
@@ -28,7 +28,10 @@ require (
 	github.com/XSAM/otelsql v0.26.0 // indirect
 	github.com/aws/aws-sdk-go v1.49.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/devigned/tab v0.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -37,14 +40,21 @@ require (
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/elastic/go-elasticsearch/v7 v7.17.10 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.5 // indirect
 	github.com/go-ldap/ldap/v3 v3.4.6 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.16.0 // indirect
 	github.com/go-redis/redis/extra/rediscmd v0.2.0 // indirect
 	github.com/go-redis/redis/extra/redisotel v0.3.0 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gocql/gocql v1.6.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
@@ -82,7 +92,10 @@ require (
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.6 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/microsoft/go-mssqldb v0.17.0 // indirect
@@ -92,6 +105,7 @@ require (
 	github.com/montanaflynn/stats v0.6.6 // indirect
 	github.com/newrelic/go-agent v3.20.2+incompatible // indirect
 	github.com/openzipkin/zipkin-go v0.4.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.17 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.17.0 // indirect
@@ -99,6 +113,8 @@ require (
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.11.1 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
@@ -120,6 +136,7 @@ require (
 	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
 	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	gofr.dev v1.0.3 // indirect
+	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/oauth2 v0.14.0 // indirect
@@ -136,6 +153,7 @@ require (
 	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.4.5 // indirect
 	gorm.io/driver/postgres v1.4.8 // indirect
 	gorm.io/driver/sqlite v1.4.4 // indirect
--- main.go
+++ main.go
@@ -1,34 +1,102 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+	// "encoding/json"
+	"fmt"
+	// "net/http"
+	"gofr.dev/pkg/errors"
+	// "github.com/gorilla/mux"
+
+	"gofr.dev/pkg/gofr"
+)
 
 type Customer struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID     int    `json:"id"`
+	Bname  string `json:"bookname"`
+	Pub    string `json:"publication"`
+	Issuer string `json:"issuername"`
+	Finee  int    `json:"fine"`
 }
 
+type CreateRequest struct {
+	ID          int     `json:"id"`
+	BookName    string  `json:"bookname" validate:"required"`
+	Publication string  `json:"publication" validate:"required"`
+	IssuerName  string  `json:"issuername" validate:"required"`
+	Fine        float64 `json:"fine" validate:"required,numeric"`
+}
+
+var req CreateRequest
+
+// func Create (ctx *gofr.Context) (interface{}, error) {
+// 	// Validate the request body
+// 	var req CreateRequest
+
+// 	// Prepare the SQL statement
+
+// 	// if err := ctx.BindJSON(&req); err != nil {
+// 	// 	ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+// 	// 	return
+// 	// }
+
+// 	var bk string = req.BookName
+// 	var pu string = req.Publication
+// 	var iss string = req.IssuerName
+// 	var fi int = int(req.Fine)
+// 	fmt.Println(bk, pu, iss, fi)
+// 	_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (bookname,publication,issuername,fine) VALUES (?,?,?,?)", bk, pu, iss, fi)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return nil, err // Return a success message
+
+// }
 func main() {
 	// initialise gofr object
 	app := gofr.New()
 
-	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-		// Get the value using the redis instance
-		value, err := ctx.Redis.Get(ctx.Context, "greeting").Result()
+	// app.POST("/customer/{name}", func(ctx *gofr.Context) (interface{}, error) {
+	// 	name := ctx.PathParam("name")
 
-		return value, err
-	})
+	// 	// Inserting a customer row in database using SQL
+	// 	_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (name) VALUES (?)", name)
 
-	app.POST("/customer/{name}", func(ctx *gofr.Context) (interface{}, error) {
-		name := ctx.PathParam("name")
+	// 	return nil, err
+	// })
+	// app.Server.UseMiddleware(customMiddleware())
+	app.POST("/customerss", func(ctx *gofr.Context) (interface{}, error) {
+		// Validate the request body
+		var req CreateRequest
+		if err := ctx.Bind(&req); err != nil {
+			ctx.Logger.Errorf("error in binding: %v", err)
+			return nil, errors.InvalidParam{Param: []string{"body"}}
+		}
+		// Prepare the SQL statement
 
-		// Inserting a customer row in database using SQL
-		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (name) VALUES (?)", name)
+		// if err := ctx.BindJSON(&req); err != nil {
+		// 	ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 
-		return nil, err
+		// 	return
+		// }
+
+		var bk string = req.BookName
+		var pu string = req.Publication
+		var iss string = req.IssuerName
+		var fi int = int(req.Fine)
+		fmt.Println(bk, pu, iss, fi)
+		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (bookname,publication,issuername,fine) VALUES (?,?,?,?)", bk, pu, iss, fi)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, err // Return a success message
+
 	})
 
 	app.GET("/customer", func(ctx *gofr.Context) (interface{}, error) {
-		var customers []Customer
+		var customers []CreateRequest
 
 		// Getting the customer from the database using SQL
 		rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM customers")
@@ -37,8 +105,8 @@ func main() {
 		}
 
 		for rows.Next() {
-			var customer Customer
-			if err := rows.Scan(&customer.ID, &customer.Name); err != nil {
+			var customer CreateRequest
+			if err := rows.Scan(&customer.ID, &customer.BookName, &customer.Publication, &customer.IssuerName, &customer.Fine); err != nil {
 				return nil, err
 			}
 
@@ -53,3 +121,20 @@ func main() {
 	// it can be over-ridden through configs
 	app.Start()
 }
+
+// func customMiddleware() func(handler http.Handler) http.Handler {
+// 	return func(inner http.Handler) http.Handler {
+// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+// 			decoder := json.NewDecoder(req.Body)
+//     		var t CreateRequest
+//     		err := decoder.Decode(&t)
+//     		if err != nil {
+//         		panic(err)
+//    			}
+//     		fmt.Println(t.BookName)// your logic here
+
+// 			// sends the request to the next middleware/request-handler
+// 			inner.ServeHTTP(w, r)
+// 		})
+// 	}
+// }
