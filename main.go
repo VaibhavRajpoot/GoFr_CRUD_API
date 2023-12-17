4ff6059d30f262d0b6551f5f7493debdccb3374a
VaibhavRajpoot <vaibhavrajpoot46gmail.com>
2023-12-17 19:44:11 +0530

    UPDATE operation completed

--- main.go
+++ main.go
@@ -26,46 +26,10 @@ type CreateRequest struct {
 	Fine        float64 `json:"fine" validate:"required,numeric"`
 }
 
-var req CreateRequest
-
-// func Create (ctx *gofr.Context) (interface{}, error) {
-// 	// Validate the request body
-// 	var req CreateRequest
-
-// 	// Prepare the SQL statement
-
-// 	// if err := ctx.BindJSON(&req); err != nil {
-// 	// 	ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-
-// 	// 	return
-// 	// }
-
-// 	var bk string = req.BookName
-// 	var pu string = req.Publication
-// 	var iss string = req.IssuerName
-// 	var fi int = int(req.Fine)
-// 	fmt.Println(bk, pu, iss, fi)
-// 	_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (bookname,publication,issuername,fine) VALUES (?,?,?,?)", bk, pu, iss, fi)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return nil, err // Return a success message
-
-// }
 func main() {
-	// initialise gofr object
+
 	app := gofr.New()
 
-	// app.POST("/customer/{name}", func(ctx *gofr.Context) (interface{}, error) {
-	// 	name := ctx.PathParam("name")
-
-	// 	// Inserting a customer row in database using SQL
-	// 	_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (name) VALUES (?)", name)
-
-	// 	return nil, err
-	// })
-	// app.Server.UseMiddleware(customMiddleware())
 	app.POST("/customerss", func(ctx *gofr.Context) (interface{}, error) {
 		// Validate the request body
 		var req CreateRequest
@@ -73,13 +37,6 @@ func main() {
 			ctx.Logger.Errorf("error in binding: %v", err)
 			return nil, errors.InvalidParam{Param: []string{"body"}}
 		}
-		// Prepare the SQL statement
-
-		// if err := ctx.BindJSON(&req); err != nil {
-		// 	ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-
-		// 	return
-		// }
 
 		var bk string = req.BookName
 		var pu string = req.Publication
@@ -87,6 +44,29 @@ func main() {
 		var fi int = int(req.Fine)
 		fmt.Println(bk, pu, iss, fi)
 		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (bookname,publication,issuername,fine) VALUES (?,?,?,?)", bk, pu, iss, fi)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, err // Return a success message
+
+	})
+
+	app.PUT("/customerss/{id}", func(ctx *gofr.Context) (interface{}, error) {
+		// Validate the request body
+		name := ctx.PathParam("id")
+		var req CreateRequest
+		if err := ctx.Bind(&req); err != nil {
+			ctx.Logger.Errorf("error in binding: %v", err)
+			return nil, errors.InvalidParam{Param: []string{"body"}}
+		}
+
+		var bk string = req.BookName
+		var pu string = req.Publication
+		var iss string = req.IssuerName
+		var fi int = int(req.Fine)
+		fmt.Println(bk, pu, iss, fi)
+		_, err := ctx.DB().ExecContext(ctx, "UPDATE customers SET bookname = ?, publication = ?, issuername =?, fine = ? WHERE id = ?", bk, pu, iss, fi, name)
 		if err != nil {
 			return nil, err
 		}
@@ -117,24 +97,5 @@ func main() {
 		return customers, nil
 	})
 
-	// Starts the server, it will listen on the default port 8000.
-	// it can be over-ridden through configs
 	app.Start()
 }
-
-// func customMiddleware() func(handler http.Handler) http.Handler {
-// 	return func(inner http.Handler) http.Handler {
-// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-// 			decoder := json.NewDecoder(req.Body)
-//     		var t CreateRequest
-//     		err := decoder.Decode(&t)
-//     		if err != nil {
-//         		panic(err)
-//    			}
-//     		fmt.Println(t.BookName)// your logic here
-
-// 			// sends the request to the next middleware/request-handler
-// 			inner.ServeHTTP(w, r)
-// 		})
-// 	}
-// }
